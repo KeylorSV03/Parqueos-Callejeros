2990443bda615531cfb911f655c9b691da47bce1
ArielST21 <arielsanchez2110@gmail.com>
2024-10-13 14:16:31 -0600

    Revert "Validacion de registro"
    This reverts commit 1c26360146573fd97bc3879e7f3f88fdad1db62a.

--- src/main/java/com/mycompany/parqueos/callejeros/Usuario.java
+++ src/main/java/com/mycompany/parqueos/callejeros/Usuario.java
@@ -1,6 +1,4 @@
 package com.mycompany.parqueos.callejeros;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 public class Usuario extends Persona{
     
@@ -10,41 +8,5 @@ public class Usuario extends Persona{
     private List<Espacio> historialEspacios;
     private List<Vehiculo> listaVehiculos;
 
-    public Usuario(String nombre, String apellido, int telefono, String correo, String direccionFisica, String PIN, String idUsuario) {
-     
-        super(nombre, apellido, telefono, correo, direccionFisica, PIN, idUsuario);
-        
-        // Asignar los valores de los atributos de Usuario
-        this.numeroTarjeta = 0; //Se debe cambiar en la configuracion del perfil
-        this.historialEspacios = new ArrayList();
-        this.listaVehiculos = new ArrayList();
-    }
 
-    // ------------------- Setters -------------------
-    
-    public void setNumeroTarjeta(int numeroTarjeta) {
-        this.numeroTarjeta = numeroTarjeta;
-    }
-
-    public void setHistorialEspacios(List<Espacio> historialEspacios) {
-        this.historialEspacios = historialEspacios;
-    }
-
-    public void setListaVehiculos(List<Vehiculo> listaVehiculos) {
-        this.listaVehiculos = listaVehiculos;
-    }
-
-    // ------------------- Getters -------------------
-
-    public int getNumeroTarjeta() {
-        return numeroTarjeta;
-    }
-
-    public List<Espacio> getHistorialEspacios() {
-        return historialEspacios;
-    }
-
-    public List<Vehiculo> getListaVehiculos() {
-        return listaVehiculos;
-    }
 }
